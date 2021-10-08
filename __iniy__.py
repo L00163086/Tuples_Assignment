c7855c4b8737d1fc55dd7446a9c217e9466b41f7
Chetan <91426296+L00163086@users.noreply.github.com>
2021-10-08 15:07:00 +0530

    Create __iniy__.py
    Init file

---
+++ __iniy__.py
@@ -0,0 +1,12 @@
+"""
+#
+ File        : __init__.py
+# Created:    : 04-10-2021 15:01
+# Author      : Chetan Raga
+# Version     : v1.0.0
+# Licencing   : (c) 2021 Chetan raga,LYIT
+               AvaiLable under GNU Public License (GPL)
+# Description :
+   
+#
+"""
